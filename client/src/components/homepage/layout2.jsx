57034a971f1b45b71d3f061f15be5d3d7cda5b99
jainpriyanshi <jainpriyanshi2407@gmail.com>
2020-03-26 19:08:11 +0545

    homepage created

--- client/src/components/homepage/layout2.jsx
+++ client/src/components/homepage/layout2.jsx
@@ -9,33 +9,35 @@ export default class layout2 extends Component {
                 <h1> Features </h1>
                 <br />
                 <br />
-                <div class="card container col-lg-8 mt-2 mb-2"> 
-                        <h3 style={{fontFamily: "roboto", color: "black"}}>Work With any Team </h3> 
-                        <b> 
+                <div class = "row"> 
+                <div class="card container col-lg-5 mt-2 mb-2 bg-info"> 
+                        <h3 class="mt-2 mb-2" style={{fontFamily: "roboto", color: "black"}}>Work With any Team </h3> 
+                        <b class="mt-2 mb-2"> 
                           Whether it’s for work, a side project or even the next family vacation, MyTrello helps your team stay organized.
                         </b>
                 </div>
-                <div class="card container col-lg-8 grey mt-2 mb-2"> 
-                        <h3 style={{fontFamily: "roboto" , color: "black"}}>Invite  Team Members </h3> 
-                        <b> 
+                <div class="card container col-lg-5 bg-success mt-2 mb-2"> 
+                        <h3  class="mt-2 mb-2" style={{fontFamily: "roboto" , color: "black"}}>Invite  Team Members </h3> 
+                        <b class="mt-2 mb-2"> 
                           Invite Members to collaborate in project and achieve faster completion of project  
                         </b>
                 </div>
-                <div class="card container col-lg-8 mt-2 mb-2"> 
-                        <h3 style={{fontFamily: "roboto" , color: "black"}}> Chat With Team Members </h3> 
-                        <b> 
+                <div class="card container bg-grey orange col-lg-5 mt-2 mb-2"> 
+                        <h3 class="mt-2 mb-2" style={{fontFamily: "roboto" , color: "black"}}> Chat With Team Members </h3> 
+                        <b class="mt-2 mb-2"> 
                            Keep Your Team Members Updated by Online Chat built using Socketio that enables instant messaging
                         </b>
                 </div>
-                <div class="card container col-lg-8 grey mt-2 mb-2"> 
-                        <h3 style={{fontFamily: "roboto", color: "black"}}> Organise </h3> 
-                        <b> 
+                <div class="card container col-lg-5 grey mt-2 mb-2"> 
+                        <h3 class="mt-2 mb-2"style={{fontFamily: "roboto", color: "black"}}> Organise </h3> 
+                        <b class="mt-2 mb-2"> 
                            Divide Your list of task into Three categories
                              Things to do,
                              Thing Doing ,
                              Things done 
                              . Moreover Divide your projects into task(List) and subtask(checklist)  
                         </b>
+                </div>
                 </div>
                 <br />
                 <br />
